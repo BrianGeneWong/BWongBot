bbecfb7a00eeb0fffa8d48acc52442e148878fde
Brian Wong <brian.gene.wong@gmail.com>
2018-10-25 21:17:55 -0400

    play works only if bot is not already in a channel

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const Discord = require('discord.js');
 const commando= require('discord.js-commando');
 const bot = new commando.Client();
+
+//token contained in this file
 const auth = require('./auth.json');
+
 
 
 bot.registry.registerGroup('random','Random');
 bot.registry.registerGroup('music','Music');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
+
+global.servers = {};
 
 bot.on("ready", () => {
     console.log("I am ready!");
